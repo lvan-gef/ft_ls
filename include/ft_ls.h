4428b519f51245f6cc3260f95dbb9bf641c0f054
Luuk van Geffen <lvan-gef@student.codam.nl>
2025-12-16 02:32:51 +0100

    Feat: move structs to ft_ls

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -1,6 +1,10 @@
 #ifndef FT_LS_H
 #define FT_LS_H
 
+#include <stdbool.h>
+
+#include "../libft/include/libft.h"
+
 #ifndef STDOUT
 #define STDOUT 1
 #endif // !STDOUT
@@ -8,5 +12,19 @@
 #ifndef STDERR
 #define STDERR 2
 #endif // !STDERR
+
+typedef struct s_arguments {
+    bool list;
+    bool recursive;
+    bool all;
+    bool reverse;
+    bool time;
+    t_list *paths;
+} t_arguments;
+
+typedef struct s_node {
+    char *path;
+} t_node;
+
 
 #endif // FT_LS_H
--- include/ft_parser.h
+++ include/ft_parser.h
@@ -3,20 +3,7 @@
 
 #include <stdbool.h>
 
-#include "../libft/include/libft.h"
-
-typedef struct s_arguments {
-    bool list;
-    bool recursive;
-    bool all;
-    bool reverse;
-    bool time;
-    t_list *paths;
-} t_arguments;
-
-typedef struct s_node {
-    char *path;
-} t_node;
+#include "../include/ft_ls.h"
 
 bool parse_args(int argc, char **argv, t_arguments *args);
 void free_args(t_arguments *args);
