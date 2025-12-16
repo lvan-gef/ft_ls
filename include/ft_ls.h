2ea5a2fc735a80e3d3827b2a0796e17ed0294362
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-12-16 22:43:08 +0100

    Feat: struct for path and file parsing

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -5,17 +5,27 @@
 
 #include "../libft/include/libft.h"
 
-typedef struct s_arguments {
+#ifndef MAX_PATH
+#define MAX_PATH 256
+#endif // !MAX_PATH
+
+typedef struct s_args {
     bool list;
     bool recursive;
     bool all;
     bool reverse;
     bool time;
     t_list *paths;
-} t_arguments;
+} t_args;
+
+typedef struct s_path {
+    char path[MAX_PATH];
+    size_t max_len;
+    t_list *files;
+} t_path;
 
 typedef struct s_node {
-    char *path;
+    char path[MAX_PATH];
 } t_node;
 
 #endif // FT_LS_H
