f336edccddd1ffdb61878f0d4083001fcc7bc7c5
Luuk van Geffen <lvan-gef@student.codam.nl>
2025-12-16 03:23:30 +0100

    Feat: Better error handling

--- src/ft_walk.c
+++ src/ft_walk.c
@@ -4,10 +4,16 @@
 #include <unistd.h>
 
 #include "../include/ft_walk.h"
+#include "../include/ft_assert.h"
 #include "../libft/include/ft_fprintf.h"
 #include "../libft/include/libft.h"
 
+
+static void clean_up_(DIR *dir);
+
 bool walk(t_arguments *args) {
+    CUSTOM_ASSERT_(args != NULL, "args can not be NULL");
+    CUSTOM_ASSERT_(args->paths != NULL, "args->paths can not be NULL");
     t_list *ll = args->paths;
 
     while (ll) {
@@ -27,6 +33,7 @@ bool walk(t_arguments *args) {
 
         if (errno) {
             ft_fprintf(STDERR_FILENO, "Failed to read the dir\n");
+            clean_up_(dir);
             return false;
         }
 
@@ -35,4 +42,10 @@ bool walk(t_arguments *args) {
     }
 
     return true;
+}
+
+static void clean_up_(DIR *dir) {
+    CUSTOM_ASSERT_(dir != NULL, "dir can not be NULL");
+
+    closedir(dir);
 }
