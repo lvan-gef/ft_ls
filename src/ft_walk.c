ef222823c3594871d4b0317ba3defa0e96401ffc
Luuk Van Geffen <lvan-gef@f0r3s1.codam.nl>
2025-12-23 20:49:14 +0100

    Feat: Get all info for a file

--- src/ft_walk.c
+++ src/ft_walk.c
@@ -1,13 +1,10 @@
 #include <dirent.h>
 #include <errno.h>
 #include <grp.h>
-#include <stdlib.h>
+#include <pwd.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
-#include <unistd.h>
-#include <pwd.h>
 
 #include "../include/ft_assert.h"
 #include "../include/ft_ls.h"
@@ -15,8 +12,14 @@
 #include "../libft/include/ft_fprintf.h"
 #include "../libft/include/libft.h"
 
-static char *parse_files(t_args *args, DIR *dir, t_path *path);
-static bool create_path_node_(t_args *args, const t_path *path, const char *pathname);
+static char *walk_files(t_args *args, DIR *dir, t_path *path);
+static char *parse_file(struct dirent *dirent, struct stat *sb, t_path *path);
+static bool create_path_node_(t_args *args, const t_path *path,
+                              const char *pathname);
+static void set_fullpath_(char *fullpath, const char *filename,
+                          const char *dir_name);
+static void get_user_group_(t_file *file, unsigned int group_id,
+                            unsigned int user_id);
 static void get_permission_(t_file *file, struct stat *sb);
 static void clean_up_(DIR *dir, t_list **paths);
 static void free_file_(void *content);
@@ -25,7 +28,7 @@ bool walk(t_args *args, t_list **paths) {
     CUSTOM_ASSERT_(args, "args can not be NULL");
     CUSTOM_ASSERT_(args->paths, "args->paths can not be NULL");
     CUSTOM_ASSERT_(paths, "paths can not be NULL");
-    // CUSTOM_ASSERT_(*paths, "*paths can not be NULL");
+    CUSTOM_ASSERT_(!*paths, "*paths must be NULL");
 
     DIR *dir = NULL;
     char *err_msg = {0};
@@ -34,7 +37,6 @@ bool walk(t_args *args, t_list **paths) {
     while (ll) {
         t_node *node = ll->content;
         dir = opendir(node->path);
-        ft_fprintf(STDOUT_FILENO, "--> %s\n", node->path);
         if (!dir) {
             err_msg = "Failed to open the dir";
             goto failed;
@@ -47,7 +49,7 @@ bool walk(t_args *args, t_list **paths) {
         }
         ft_strlcpy(path->path, node->path, MAX_PATH);
 
-        err_msg = parse_files(args, dir, path);
+        err_msg = walk_files(args, dir, path);
         if (*err_msg) {
             goto failed;
         }
@@ -78,23 +80,26 @@ void free_walk(void *content) {
     free(path);
 }
 
-static char *parse_files(t_args *args, DIR *dir, t_path *path) {
+// TODO: . and .. we need that for ls even if not in recursive mode
+static char *walk_files(t_args *args, DIR *dir, t_path *path) {
     CUSTOM_ASSERT_(args, "args can not be NULL");
     CUSTOM_ASSERT_(dir, "dir can not be NULL");
     CUSTOM_ASSERT_(path, "path can not be NULL");
 
     errno = 0;
     struct dirent *dirent = readdir(dir);
-    t_list *node = NULL;
 
     while (dirent && !errno) {
         struct stat sb;
 
-        char fullpath[MAX_PATH] = {0};
+        if (*dirent->d_name == '.' && !args->all) {
+            dirent = readdir(dir);
+            continue;
+        }
 
-        ft_strlcpy(fullpath, path->path, MAX_PATH);
-        ft_strlcat(fullpath, "/", MAX_PATH);
-        ft_strlcat(fullpath, dirent->d_name, MAX_PATH);
+        char fullpath[MAX_PATH] = {0};
+        set_fullpath_(fullpath, path->path, dirent->d_name);
+
         if (lstat(fullpath, &sb) == -1) {
             return strerror(errno);
         }
@@ -108,48 +113,10 @@ static char *parse_files(t_args *args, DIR *dir, t_path *path) {
             continue;
         }
 
-        const size_t len = ft_strlen(dirent->d_name);
-        if (*dirent->d_name == '.' && !args->all) {
-            dirent = readdir(dir);
-            continue;
+        char *parse_error = parse_file(dirent, &sb, path);
+        if (parse_error) {
+            return parse_error;
         }
-
-        if (len > path->max_len) {
-            path->max_len = len;
-        }
-
-        t_file *file = ft_calloc(1, sizeof(*file));
-        if (!file) {
-            return strerror(errno);
-        }
-
-        struct group *grp = getgrgid(sb.st_gid);
-        if (grp) {
-            ft_strlcpy(file->group, grp->gr_name, MAX_PATH);
-        } else {
-            // TODO: handle error like ls
-        }
-
-        struct passwd *pwd = getpwuid(sb.st_uid);
-        if (pwd) {
-            ft_strlcpy(file->user, pwd->pw_name, MAX_PATH);
-        } else {
-            // TODO: handle error like ls
-        }
-
-        file->size = sb.st_size;
-        ft_strlcpy(file->filename, dirent->d_name, MAX_PATH);
-        get_permission_(file, &sb);
-        file->hardlink = sb.st_nlink;
-        ft_fprintf(STDOUT_FILENO, "%s %d %s %s %d %s\n", file->permission,
-                   file->hardlink, file->user, file->group, file->size, file->filename);
-
-        node = ft_lstnew((void *)file);
-        if (!node) {
-            free(file);
-            return strerror(errno);
-        }
-        ft_lstadd_back(&path->files, node);
 
         dirent = readdir(dir);
     }
@@ -161,7 +128,47 @@ static char *parse_files(t_args *args, DIR *dir, t_path *path) {
     return "\0";
 }
 
-static bool create_path_node_(t_args *args, const t_path *path, const char *pathname) {
+static char *parse_file(struct dirent *dirent, struct stat *sb, t_path *path) {
+    CUSTOM_ASSERT_(dirent, "dirent can not be NULL");
+    CUSTOM_ASSERT_(sb, "sb can not be NULL");
+    CUSTOM_ASSERT_(path, "path can not be NULL");
+
+    t_list *node = NULL;
+
+    const size_t len = ft_strlen(dirent->d_name);
+    if (len > path->max_len) {
+        path->max_len = len;
+    }
+
+    t_file *file = ft_calloc(1, sizeof(*file));
+    if (!file) {
+        return strerror(errno);
+    }
+
+    ft_strlcpy(file->date, ctime(&sb->st_atim.tv_sec) + 4, DT_LEN);
+    file->size = sb->st_size;
+    ft_strlcpy(file->filename, dirent->d_name, MAX_PATH);
+    file->hardlink = sb->st_nlink;
+    get_permission_(file, sb);
+    get_user_group_(file, sb->st_gid, sb->st_uid);
+
+    node = ft_lstnew((void *)file);
+    if (!node) {
+        free(file);
+        return strerror(errno);
+    }
+    ft_lstadd_back(&path->files, node);
+
+    return "\0";
+}
+
+static bool create_path_node_(t_args *args, const t_path *path,
+                              const char *pathname) {
+    CUSTOM_ASSERT_(args, "args can not be NULL");
+    CUSTOM_ASSERT_(path, "path can not be NULL");
+    CUSTOM_ASSERT_(pathname, "pathname can not be NULL");
+    CUSTOM_ASSERT_(*pathname != '\0', "*pathname can not be '\\0'");
+
     if (*pathname == '.' && !args->all) {
         return true;
     }
@@ -189,7 +196,45 @@ static bool create_path_node_(t_args *args, const t_path *path, const char *path
     return true;
 }
 
+static void set_fullpath_(char *fullpath, const char *filename,
+                          const char *dir_name) {
+    CUSTOM_ASSERT_(fullpath, "fullpath can not be NULL");
+    CUSTOM_ASSERT_(*fullpath == '\0', "*fullpath must be '\\0'");
+    CUSTOM_ASSERT_(filename, "filename can not be NULL");
+    CUSTOM_ASSERT_(*filename != '\0', "*filename can not be '\\0'");
+    CUSTOM_ASSERT_(dir_name, "dir_name can not be NULL");
+    CUSTOM_ASSERT_(*dir_name != '\0', "*dir_name can not be '\\0'");
+
+    ft_strlcpy(fullpath, filename, MAX_PATH);
+    ft_strlcat(fullpath, "/", MAX_PATH);
+    ft_strlcat(fullpath, dir_name, MAX_PATH);
+}
+
+static void get_user_group_(t_file *file, unsigned int group_id,
+                            unsigned int user_id) {
+    CUSTOM_ASSERT_(file, "file can no be NULL");
+    CUSTOM_ASSERT_(group_id > 0, "group_id must be more then 0");
+    CUSTOM_ASSERT_(user_id > 0, "user_id must be more then 0");
+
+    struct group *grp = getgrgid(group_id);
+    if (grp) {
+        ft_strlcpy(file->group, grp->gr_name, MAX_PATH);
+    } else {
+        // TODO: handle error like ls
+    }
+
+    struct passwd *pwd = getpwuid(user_id);
+    if (pwd) {
+        ft_strlcpy(file->user, pwd->pw_name, MAX_PATH);
+    } else {
+        // TODO: handle error like ls
+    }
+}
+
 static void get_permission_(t_file *file, struct stat *sb) {
+    CUSTOM_ASSERT_(file, "file can not be NULL");
+    CUSTOM_ASSERT_(sb, "sb can not be NULL");
+
     switch (sb->st_mode & S_IFMT) {
         case S_IFLNK:
             ft_fprintf(STDOUT_FILENO, "symbolic link\n");
