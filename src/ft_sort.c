0d63ad627ca7c096d5cb4bf570a8cf0d8e1aa6c0
Luuk Van Geffen <lvan-gef@f1r2s2.codam.nl>
2026-01-20 18:00:53 +0100

    Feat: This is my best i can do with sorting...

--- src/ft_sort.c
+++ src/ft_sort.c
@@ -1,12 +1,14 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <unistd.h>
 
 #include "../include/ft_array.h"
 #include "../include/ft_assert.h"
 #include "../include/ft_ls.h"
 #include "../include/ft_sort.h"
+#include "../libft/include/libft.h"
 
-static int comapre_(const char *a, const char *b);
+static int compare_(const char *a, const char *b);
 
 void sort_alpha(t_array *files) {
     ASSERT_(files, "files can not be NULL");
@@ -18,7 +20,7 @@ void sort_alpha(t_array *files) {
         while (sub_index < files->len) {
             t_file *file_a = (t_file *)files->data[index];
             t_file *file_b = (t_file *)files->data[sub_index];
-            int result = comapre_(file_a->filename, file_b->filename);
+            int result = compare_(file_a->filename, file_b->filename);
             if (result > 0) {
                 files->data[sub_index] = file_a;
                 files->data[index] = file_b;
@@ -36,12 +38,19 @@ void sort_time(t_array *files, bool reverse) {
     (void)reverse;
 }
 
-static int comapre_(const char *a, const char *b) {
-    ASSERT_(a, "a can not be NULL");
-    ASSERT_(*a, "*a can not be '\\0'");
-    ASSERT_(b, "b can not be NULL");
-    ASSERT_(*b, "*b can not be '\\0'");
+static int get_priority(char c) {
+    if (ft_isalpha(c)) {
+        return 0;
+    }
 
+    if (ft_isdigit(c)) {
+        return 1;
+    }
+
+    return 2;
+}
+
+static int compare_(const char *a, const char *b) {
     if (*a == '\'' || *a == '"') {
         ++a;
     }
@@ -50,10 +59,58 @@ static int comapre_(const char *a, const char *b) {
         ++b;
     }
 
-    while (*a && *b && *a == *b) {
+    const char *a_clean = a;
+    const char *b_clean = b;
+    while (*a || *b) {
+        while (*a && !ft_isalpha(*a)) {
+            ++a;
+        }
+
+        while (*b && !ft_isalpha(*b)) {
+            ++b;
+        }
+
+        if (!*a || !*b) {
+            break;
+        }
+
+        if (ft_tolower(*a) != ft_tolower(*b)) {
+            return ft_tolower(*a) - ft_tolower(*b);
+        }
+
         ++a;
         ++b;
     }
 
-    return (unsigned char)*a - (unsigned char)*b;
+    while (*a && !ft_isalpha(*a)) {
+        ++a;
+    }
+
+    while (*b && !ft_isalpha(*b)) {
+        ++b;
+    }
+
+    if (*a || *b) {
+        return ft_isalpha(*a) ? 1 : -1;
+    }
+
+    a = a_clean;
+    b = b_clean;
+    while (*a && *b) {
+        int pa = get_priority(*a);
+        int pb = get_priority(*b);
+
+        if (pa != pb) {
+            return pa - pb;
+        }
+
+        if (ft_tolower(*a) != ft_tolower(*b)) {
+            return ft_tolower(*a) - ft_tolower(*b);
+        }
+
+        ++a;
+        ++b;
+    }
+
+    return *a - *b;
 }
