abf9cec67aceb26a56179beecbcda78fbfc40fce
Luuk van Geffen <lvan-gef@student.codam.nl>
2026-01-29 22:03:24 +0100

    Feat: ls -l impeld

---
+++ include/ft_helpers.h
@@ -0,0 +1,8 @@
+#ifndef FT_HELPERS_H
+#define FT_HELPERS_H
+
+#include <stddef.h>
+
+size_t uitoa(char *buffer, size_t buffer_len, size_t n);
+
+#endif // !FT_HELPERS_H
--- src/ft_print.c
+++ src/ft_print.c
@@ -1,22 +1,28 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #include "../include/ft_arena.h"
 #include "../include/ft_array.h"
 #include "../include/ft_assert.h"
+#include "../include/ft_helpers.h"
 #include "../include/ft_ls.h"
 #include "../include/ft_print.h"
 #include "../include/ft_sort.h"
 
 #include "../libft/include/ft_fprintf.h"
 #include "../libft/include/libft.h"
+#include "ft_printf.h"
 
-static void printer_(const t_args *args, t_path *path);
-static void print_(Arena *arena, t_path *path);
+static void printer_(Arena *arena, const t_args *args, t_path *path);
+static void print_row(Arena *arena, t_path *path);
+static void print_list_(Arena *arena, t_path *path);
 static bool calc_cols_(Arena *arena, t_path *path, size_t **col_widths,
                        size_t *num_cols, size_t *num_rows);
+static size_t list_str_len_(t_path *path, size_t **lens, size_t *total);
+static char *rigth_pad(Arena *arena, size_t nbr, size_t nbr_len);
 #if defined(__linux__)
 static size_t calc_layout_width_(t_array *files, size_t num_cols,
                                  size_t *col_widths, bool quoted);
@@ -33,15 +39,16 @@ void print_ls(t_args *args) {
         return;
     }
 
+    Arena *arena = ArenaAlloc(4096);
     size_t index = 0;
     while (index < paths->len) {
         t_path *path = paths->data[index];
-        printer_(args, path);
+        printer_(arena, args, path);
         ++index;
     }
 }
 
-static void printer_(const t_args *args, t_path *path) {
+static void printer_(Arena *arena, const t_args *args, t_path *path) {
     ASSERT_(args, "args can not be NULL");
     ASSERT_(path, "path can not be NULL");
     ASSERT_(path->files, "path->files can not be NULL");
@@ -58,17 +65,16 @@ static void printer_(const t_args *args, t_path *path) {
         sort_alpha(path->files, args->reverse);
     }
 
-    Arena *arena = ArenaAlloc(4096);
     if (args->list) {
-        // TODO: implement list view
+        print_list_(arena, path);
     } else {
-        print_(arena, path);
+        print_row(arena, path);
     }
 
     ArenaRelease(arena);
 }
 
-static void print_(Arena *arena, t_path *path) {
+static void print_row(Arena *arena, t_path *path) {
     ASSERT_(arena, "arena can not be NULL");
     ASSERT_(path, "path can not be NULL");
     ASSERT_(path->files, "path->files can not be NULL");
@@ -88,11 +94,12 @@ static void print_(Arena *arena, t_path *path) {
 
     size_t *col_starts = ArenaPush(arena, (num_cols + 1) * sizeof(*col_starts));
     if (!col_starts) {
+        ft_fprintf(STDERR_FILENO, "Failed to alloc memory in arena\n");
         return;
     }
 
     col_starts[0] = 0;
-    for (size_t c = 0; c < num_cols; c++) {
+    for (size_t c = 0; c < num_cols; ++c) {
         col_starts[c + 1] = col_starts[c] + col_widths[c] + 2;
     }
 
@@ -104,11 +111,11 @@ static void print_(Arena *arena, t_path *path) {
     }
 
     const size_t files_len = path->files->len;
-    for (size_t row = 0; row < num_rows; row++) {
+    for (size_t row = 0; row < num_rows; ++row) {
         size_t buf_len = 0;
         size_t cur_pos = 0;
 
-        for (size_t col = 0; col < num_cols; col++) {
+        for (size_t col = 0; col < num_cols; ++col) {
             size_t idx = row + col * num_rows;
             if (idx >= files_len) {
                 break;
@@ -127,7 +134,7 @@ static void print_(Arena *arena, t_path *path) {
 
             buf_len +=
                 ft_strlcpy(buf + buf_len, f->filename, buf_size - buf_len);
-            cur_pos += f->len;
+            cur_pos += f->filename_len;
 
             if (!is_last_col) {
                 size_t target_pos = col_starts[col + 1];
@@ -142,7 +149,7 @@ static void print_(Arena *arena, t_path *path) {
                     }
 
                     test_pos = next_tab;
-                    num_tabs++;
+                    ++num_tabs;
                 }
 
                 size_t spaces_after_tabs = target_pos - test_pos;
@@ -207,7 +214,7 @@ static bool calc_cols_(Arena *arena, t_path *path, size_t **col_widths,
     for (size_t try_cols = max_cols; try_cols > 1; --try_cols) {
         size_t width = calc_layout_width_(path->files, try_cols, *col_widths,
                                           path->quoted);
-        if (width <= TERM_SIZE) {
+        if (width < TERM_SIZE) {
             *num_cols = try_cols;
             *num_rows = (files_len + *num_cols - 1) / *num_cols;
             break;
@@ -253,6 +260,7 @@ static size_t calc_layout_width_(t_array *files, size_t num_cols,
     ASSERT_(files, "files can not be NULL");
     ASSERT_(files->len, "files->len must be > 0");
     ASSERT_(files->data, "files->data can not be NULL");
+    ASSERT_(files->data[0], "files->data[0] can not be NULL");
     ASSERT_(num_cols, "num_cols musr be > 0");
     ASSERT_(col_widths, "col_widths can not be NULL");
 
@@ -268,10 +276,11 @@ static size_t calc_layout_width_(t_array *files, size_t num_cols,
             if (index >= files->len) {
                 break;
             }
+
             const t_file *f = files->data[index];
             ASSERT_(f, "f can not be NULL");
-            ASSERT_(f->len, "f->len can not be NULL");
-            size_t len = f->len;
+            ASSERT_(f->filename_len, "f->len can not be NULL");
+            size_t len = f->filename_len;
             if (quoted && !(f->filename[0] == '"' || f->filename[0] == '\'')) {
                 ASSERT_(len + 1 > len, "len did overflow");
                 len += 1;
@@ -284,7 +293,7 @@ static size_t calc_layout_width_(t_array *files, size_t num_cols,
     }
 
     size_t total = 0;
-    for (size_t c = 0; c < num_cols; c++) {
+    for (size_t c = 0; c < num_cols; ++c) {
         total += col_widths[c];
         if (c < num_cols - 1) {
             ASSERT_(total + 2 > total, "total did overflow");
@@ -295,3 +304,202 @@ static size_t calc_layout_width_(t_array *files, size_t num_cols,
     return total;
 }
 #endif
+
+static void print_list_(Arena *arena, t_path *path) {
+    ASSERT_(arena, "arena can not be NULL");
+    ASSERT_(path, "path can not be NULL");
+    ASSERT_(path->files, "path->files can not be NULL");
+    ASSERT_(path->files->len, "path->files->len must be > 0");
+    ASSERT_(path->files->data, "path->files->data can not be NULL");
+    ASSERT_(path->files->data[0], "path->files->data[0] can not be NULL");
+    ASSERT_(path->max_len, "path->max_len must be > 0");
+
+    size_t *lens = ArenaPush(arena, 7 * sizeof(*lens));
+    if (!lens) {
+        // TODO: print error
+        return;
+    }
+
+    size_t total = 0;
+    size_t char_count = list_str_len_(path, &lens, &total);
+    if (!char_count) {
+        return;
+    }
+
+    size_t str_len = char_count * sizeof(char);
+    Arena *scratch_arena = ArenaAlloc(str_len);
+    if (!scratch_arena) {
+        return;
+    }
+
+    char *total_str = ArenaPush(scratch_arena, str_len);
+    if (!total_str) {
+        goto failed;
+    }
+
+    size_t len = ft_strlcpy(total_str, "total ", str_len);
+    len += uitoa(total_str + len, str_len, total / 2);
+    len += ft_strlcpy(total_str + len, "\n", str_len);
+    if (write(STDOUT_FILENO, total_str, len) < 0) {
+        goto failed;
+    }
+    ArenaClear(scratch_arena);
+
+    size_t index = 0;
+    while (index < path->files->len) {
+        t_file *file = path->files->data[index];
+        len = ft_strlcpy(total_str, file->permission, str_len);
+        len += ft_strlcpy(total_str + len, " ", str_len);
+
+        char *hardlink = rigth_pad(arena, file->hardlink, lens[1]);
+        if (!hardlink) {
+            goto failed;
+        }
+        len += ft_strlcpy(total_str + len, hardlink, str_len);
+        len += ft_strlcpy(total_str + len, " ", str_len);
+
+        size_t group_len = ft_strlcpy(total_str + len, file->group, str_len);
+        len += group_len;
+        while (group_len < lens[2]) {
+            len += ft_strlcpy(total_str + len, " ", str_len);
+            ++group_len;
+        }
+        len += ft_strlcpy(total_str + len, " ", str_len);
+
+        size_t user_len = ft_strlcpy(total_str + len, file->user, str_len);
+        len += user_len;
+        while (user_len < lens[3]) {
+            len += ft_strlcpy(total_str + len, " ", str_len);
+            ++user_len;
+        }
+        len += ft_strlcpy(total_str + len, " ", str_len);
+
+        char *size = rigth_pad(arena, (size_t)file->size, lens[4]);
+        if (!size) {
+            goto failed;
+        }
+        len += ft_strlcpy(total_str + len, size, str_len);
+        len += ft_strlcpy(total_str + len, " ", str_len);
+
+        len += ft_strlcpy(total_str + len, file->date_fmt, str_len);
+        len += ft_strlcpy(total_str + len, " ", str_len);
+
+        if (!(file->filename[0] == '\'' || file->filename[0] == '"')) {
+            len += ft_strlcpy(total_str + len, " ", str_len);
+        }
+        len += ft_strlcpy(total_str + len, file->filename, str_len);
+
+        len += ft_strlcpy(total_str + len, "\n", str_len);
+        if (write(STDOUT_FILENO, total_str, len) < 0) {
+            goto failed;
+        }
+
+        ++index;
+        ArenaClear(scratch_arena);
+    }
+
+    ArenaRelease(scratch_arena);
+    return;
+failed:
+    // TODO: print error
+    ArenaRelease(scratch_arena);
+}
+
+static size_t list_str_len_(t_path *path, size_t **lens, size_t *total) {
+    size_t index = 0;
+    size_t char_len = 1024 * sizeof(char);
+    Arena *scratch_arena = ArenaAlloc(char_len * 2);
+    while (index < path->files->len) {
+        t_file *file = path->files->data[index];
+        size_t permission_len = ft_strlen(file->permission);
+        if (permission_len > (*lens)[0]) {
+            (*lens)[0] = permission_len;
+        }
+
+        char *str = ArenaPush(scratch_arena, char_len);
+        if (!str) {
+            goto failed;
+            return 0;
+        }
+
+        size_t hardlink_len = uitoa(str, char_len, file->hardlink);
+        if (hardlink_len > (*lens)[1]) {
+            (*lens)[1] = hardlink_len;
+        }
+
+        size_t group_len = ft_strlen(file->group);
+        if (group_len > (*lens)[2]) {
+            (*lens)[2] = group_len;
+        }
+
+        size_t user_len = ft_strlen(file->user);
+        if (user_len > (*lens)[3]) {
+            (*lens)[3] = user_len;
+        }
+
+        str = ArenaPush(scratch_arena, char_len);
+        if (!str) {
+            goto failed;
+        }
+
+        size_t size_len = uitoa(str, char_len, (size_t)file->size);
+        if (size_len > (*lens)[4]) {
+            (*lens)[4] = size_len;
+        }
+        ASSERT_(*total <= SIZE_MAX - file->blocks, "total did overflow");
+        *total += file->blocks;
+
+        size_t date_len = ft_strlen(file->date_fmt);
+        if (date_len > (*lens)[5]) {
+            (*lens)[5] = date_len;
+        }
+
+        size_t filename_len = ft_strlen(file->filename);
+        if (filename_len > (*lens)[6]) {
+            (*lens)[6] = filename_len;
+        }
+
+        ++index;
+        ArenaClear(scratch_arena);
+    }
+
+    size_t str_len = 0;
+    index = 0;
+
+    while (index < 7) {
+        str_len += (*lens)[index] + 2;
+        ++index;
+    }
+
+    ArenaRelease(scratch_arena);
+    return str_len;
+failed:
+    // TODO: print error
+    ArenaRelease(scratch_arena);
+    return 0;
+}
+
+static char *rigth_pad(Arena *arena, size_t nbr, size_t nbr_len) {
+    char *nbr_str = ArenaPush(arena, nbr_len + 1);
+    if (!nbr_str) {
+        return NULL;
+    }
+
+    size_t str_len = uitoa(nbr_str, nbr_len + 1, nbr);
+    U64 arena_pos = ArenaPos(arena);
+    char *buffer = ArenaPush(arena, nbr_len + 1);
+    if (!buffer) {
+        ArenaPopTo(arena, arena_pos);
+        return NULL;
+    }
+
+    size_t pad_count = (nbr_len > str_len) ? nbr_len - str_len : 0;
+    size_t len = 0;
+    while (len < pad_count) {
+        buffer[len] = ' ';
+        ++len;
+    }
+
+    ft_strlcpy(buffer + len, nbr_str, nbr_len + 1 - len);
+    return buffer;
+}
