2edf624fb2bb58b729913db858a479ca11a1a6f3
Luuk Van Geffen <lvan-gef@f0r5s3.codam.nl>
2026-02-24 21:53:22 +0100

    Feat: start impl path info

--- include/ft_path.h
+++ include/ft_path.h
@@ -1,17 +1,51 @@
 #ifndef FT_PATH_H
 #define FT_PATH_H
 
+#include <stdbool.h>
 #include <sys/stat.h>
+#include <limits.h>
+#include <linux/limits.h>
 
 #include "ft_str.h"
+
+#ifndef LOGIN_NAME_MAX
+#define LOGIN_NAME_MAX INT64_C(256)
+#endif // ifndef LOGIN_NAME_MAX //
+
+#ifndef PATH_MAX
+#define PATH_MAX INT64_C(4096)
+#endif // ifndef PATH_MAX //
+
+#ifndef PERMISSION_SIZE
+#define PERMISSION_SIZE UINT64_C(12)
+#endif // ifndef PERMISSION_SIZE //
+
+#ifndef DT_LEN
+#define DT_LEN UINT64_C(13)
+#endif // ifndef DT_LEN //
+
+#ifndef ROW_ELEMENTS
+#define ROW_ELEMENTS UINT64_C(8)
+#endif // ifndef ROW_ELEMENTS //
+
+typedef struct {
+    t_str *perm;
+    t_str *links;
+    t_str *username;
+    t_str *groupname;
+    t_str *size;
+    t_str *symlink;
+} t_file_info;
 
 typedef struct {
     t_str *name;
     t_str *path;
     t_str *quoted;
     struct stat st;
+    t_file_info *info;
 } t_entry;
 
 t_str *get_path_entry(Arena *arena, t_entry *entry);
+bool get_file_info(Arena *arena, t_entry *entry);
 
 #endif // !FT_PATH_H
--- src/ft_path.c
+++ src/ft_path.c
@@ -1,12 +1,36 @@
 #include <stdint.h>
+#include <sys/stat.h>
 
-#include "../include/ft_path.h"
 #include "../include/ft_arena.h"
 #include "../include/ft_assert.h"
+#include "../include/ft_path.h"
 #include "../include/ft_str.h"
 
 #include "../libft/include/libft.h"
+#include "ft_fprintf.h"
 
+static t_str *get_perm_(Arena *arena, t_entry *entry);
+
+typedef enum {
+    P_LINK,
+    P_REG,
+    P_DIR,
+    P_RUSER,
+    P_WUSER,
+    P_XUSER,
+    P_RGROUP,
+    P_WGROUP,
+    P_XGROUP,
+    P_ROTHER,
+    P_WOTHER,
+    P_XOTHER,
+    P_TOTAL
+} t_perm_lttr;
+
+// static uid_t cached_uid = (uid_t)-1;
+// static gid_t cached_gid = (gid_t)-1;
+// static char cached_user[LOGIN_NAME_MAX] = "";
+// static char cached_group[LOGIN_NAME_MAX] = "";
 
 t_str *get_path_entry(Arena *arena, t_entry *entry) {
     ASSERT_NOTNULL(entry);
@@ -37,6 +61,124 @@ t_str *get_path_entry(Arena *arena, t_entry *entry) {
 
     return new_str;
 
+failed:
+    if (str) {
+        ArenaPopTo(arena, arena_pos);
+    }
+
+    return NULL;
+}
+
+bool get_file_info(Arena *arena, t_entry *entry) {
+    ASSERT_NOTNULL(entry);
+    ASSERT_NOTNULL(entry->path);
+    ASSERT_GT(entry->path->cap, 0);
+    ASSERT_LT(entry->path->len, entry->path->cap);
+    ASSERT_NOTNULL(arena);
+
+    const uint64_t arena_pos = ArenaPos(arena);
+    t_file_info *info = ArenaPush(arena, sizeof(*info));
+    if (!info) {
+        goto failed;
+    }
+
+    info->perm = get_perm_(arena, entry);
+    if (!info->perm) {
+        goto failed;
+    }
+
+    info->links = uint_to_str(arena, entry->st.st_nlink);
+    if (!info->links) {
+        goto failed;
+    }
+
+    ft_fprintf(STDOUT_FILENO, "%s %s\n", info->perm->str, info->links->str);
+
+    entry->info = info;
+    return true;
+
+failed:
+    if (info) {
+        ArenaPopTo(arena, arena_pos);
+    }
+
+    return false;
+}
+
+static t_str *get_perm_(Arena *arena, t_entry *entry) {
+    const uint64_t arena_pos = ArenaPos(arena);
+
+    t_str *str = init_str(arena, PERMISSION_SIZE);
+    if (!str) {
+        goto failed;
+    }
+
+    Arena *scratch = ArenaAlloc(ARENA_SIZE);
+    if (!scratch) {
+        goto failed;
+    }
+    ArenaSetAutoAlign(arena, 8);
+
+    for (uint64_t index = 0; index < P_TOTAL; ++index) {
+        switch (index) {
+            case P_LINK:
+                if ((entry->st.st_mode & S_IFMT) == S_IFLNK) {
+                    append_chars_str(scratch, str, "l");
+                }
+                break;
+            case P_REG:
+                if ((entry->st.st_mode & S_IFMT) == S_IFREG) {
+                    append_chars_str(scratch, str, "-");
+                }
+                break;
+
+            case P_DIR:
+                if ((entry->st.st_mode & S_IFMT) == S_IFDIR) {
+                    append_chars_str(scratch, str, "d");
+                }
+                break;
+            case P_RUSER:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IRUSR ? "r" : "-");
+                break;
+            case P_WUSER:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IWUSR ? "w" : "-");
+                break;
+            case P_XUSER:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IXUSR ? "x" : "-");
+                break;
+            case P_RGROUP:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IRGRP ? "r" : "-");
+                break;
+            case P_WGROUP:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IWGRP ? "w" : "-");
+                break;
+            case P_XGROUP:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IXGRP ? "x" : "-");
+                break;
+            case P_ROTHER:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IROTH ? "r" : "-");
+                break;
+            case P_WOTHER:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IWOTH ? "w" : "-");
+                break;
+            case P_XOTHER:
+                append_chars_str(scratch, str,
+                                 entry->st.st_mode & S_IXOTH ? "x" : "-");
+                break;
+            default: ASSERT_FALSE(true);
+        }
+    }
+
+    ArenaRelease(scratch);
+    return str;
 failed:
     if (str) {
         ArenaPopTo(arena, arena_pos);
