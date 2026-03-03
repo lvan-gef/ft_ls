b16c64b699a2d497a3389859c07a9a63a72888de
Luuk Van Geffen <lvan-gef@f0r3s11.codam.nl>
2026-03-03 21:54:41 +0100

    Feat: use t_entry instead of t_str for path

--- include/ft_printer.h
+++ include/ft_printer.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 
 #include "./ft_parse.h"
-#include "./ft_str.h"
+#include "./ft_path.h"
 
-void printer(const t_args *args, t_array *array, const t_str *dir_path,
+void printer(const t_args *args, t_array *array, t_entry *dir_entry,
              bool print_total, uint64_t min_len_links, uint64_t min_len_sizes);
 
 #endif // !FT_PRINTER_H
