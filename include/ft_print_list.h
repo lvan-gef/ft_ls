7abab1b47caa740a0d2d87e7db558300ff4a2573
Luuk van Geffen <lvan-gef@student.codam.nl>
2026-03-10 02:14:12 +0100

    Format code

--- include/ft_print_list.h
+++ include/ft_print_list.h
@@ -8,6 +8,7 @@
 #include "./ft_entry.h"
 
 void print_list(t_array *array, t_entry *dir_entry, bool print_total,
-                uint64_t min_len_links, uint64_t min_len_sizes);
+                uint64_t min_len_links, uint64_t min_len_sizes,
+                bool force_quote_padding);
 
 #endif // !FT_PRINT_LIST_H
