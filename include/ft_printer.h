75a95e13f01ba01c6b9ce4f22371208678a6b18b
Luuk van Geffen <lvan-gef@student.codam.nl>
2026-03-10 02:14:26 +0100

    Format code

--- include/ft_printer.h
+++ include/ft_printer.h
@@ -3,10 +3,11 @@
 
 #include <stdint.h>
 
-#include "./ft_parse.h"
 #include "./ft_entry.h"
+#include "./ft_parse.h"
 
 void printer(const t_args *args, t_array *array, t_entry *dir_entry,
-             bool print_total, uint64_t min_len_links, uint64_t min_len_sizes);
+             bool print_total, uint64_t min_len_links, uint64_t min_len_sizes,
+             bool force_quote_padding);
 
 #endif // !FT_PRINTER_H
