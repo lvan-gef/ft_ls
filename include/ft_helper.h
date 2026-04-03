5a5ca92b513dbc53bf0c9bb41fe0fb30b3dc3262
Luuk Van Geffen <lvan-gef@f0r5s5.codam.nl>
2026-04-03 20:46:42 +0200

    Feat: remove shell escape functions

--- include/ft_helper.h
+++ include/ft_helper.h
@@ -1,20 +1,8 @@
 #ifndef FT_HELPER_H
 #define FT_HELPER_H
 
-#include <stdbool.h>
 #include <stdint.h>
 
-#include "./ft_free_list.h"
-#include "./ft_str.h"
-
-typedef bool (*ft_write_mem_fn)(void *ctx, const char *src, uint64_t len);
-
 uint64_t len_of_nbr(uint64_t nbr);
-char shell_quote_style(const t_str *str);
-bool has_shell_quote_char(const t_str *str);
-uint64_t shell_display_len(const t_str *str, char quote, bool pad_unquoted);
-bool write_shell_escaped(void *ctx, ft_write_mem_fn writer, const t_str *str,
-                         char quote, bool pad_unquoted);
-t_str *shell_escape_str(free_list *fl, const t_str *str, char quote);
 
 #endif // !FT_HELPER_H
