9bc92e1535185b8ac3cdce9cd8b92fd5b5fc5b10
Luuk Van Geffen <lvan-gef@f0r5s5.codam.nl>
2026-04-03 20:50:00 +0200

    Fix: use same name for decl and impl

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -189,7 +189,7 @@ void *ft_memmove(void *dst, const void *src, size_t len);
  *
  * @return void*
  */
-void *ft_memset(void *b, int c, size_t len);
+void *ft_memset(void *dst, int val, size_t len);
 
 /**
  * @brief Write a char
