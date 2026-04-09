c00e5917bb62873ad254c298a44cfd93af2667d3
Luuk Van Geffen <lvan-gef@f0r1s1.codam.nl>
2026-04-09 21:21:39 +0200

    Add: more test cases

--- tester/gen_data.py
+++ tester/gen_data.py
@@ -3,23 +3,53 @@ from itertools import combinations
 from typing import Generator
 
 
-ALLOWED_FLAGS = ("R", "a", "l", "r", "t")
+ALLOWED_FLAGS = ('R', 'a', 'l', 'r', 't')
 
 
 def gen_data(paths: Paths) -> Generator[list[str], None, None]:
     for size in range(1, len(ALLOWED_FLAGS) + 1):
         for combo in combinations(ALLOWED_FLAGS, size):
-            flags = "-" + "".join(combo)
+            flags = '-' + ''.join(combo)
 
+            # paths combies with -- and without
             for entry in paths.paths:
                 yield [flags, str(entry)]
 
+            for entry in paths.paths:
+                yield ['--', flags, str(entry)]
+
+            for entry in paths.paths:
+                yield [flags, '--', str(entry)]
+
+            # files combies with -- and without
             for entry in paths.files:
                 yield [flags, str(entry)]
 
+            for entry in paths.files:
+                yield ['--', flags, str(entry)]
+
+            for entry in paths.files:
+                yield [flags, '--', str(entry)]
+
+            # path and files combies with -- and without
             for dir_entry in paths.paths:
                 for file_entry in paths.files:
                     yield [flags, str(dir_entry), str(file_entry)]
 
+            for dir_entry in paths.paths:
+                for file_entry in paths.files:
+                    yield ['--', flags, str(dir_entry), str(file_entry)]
+
+            for dir_entry in paths.paths:
+                for file_entry in paths.files:
+                    yield [flags, '--', str(dir_entry), str(file_entry)]
+
+            # some edge cases with -- and without
             for case in paths.cases:
                 yield [flags, *(str(entry) for entry in case)]
+
+            for case in paths.cases:
+                yield [flags, '--', *(str(entry) for entry in case)]
+
+            for case in paths.cases:
+                yield ['--', flags, *(str(entry) for entry in case)]
